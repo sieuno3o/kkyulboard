630500440f3a055259717697f80f026e7453e4fb
heekyjung <heekyjung@gmail.com>
2024-04-01 21:47:18 +0900

    feat create_post, my_info

--- app/acc/routes.py
+++ app/acc/routes.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template
 
 acc_bp = Blueprint('acc', __name__, url_prefix='/acc')
 
+
 @acc_bp.route('/index')
 def index():
     return render_template('acc/index.html')
+
+
+@acc_bp.route('/my_info')
+def my_info():
+    return render_template('acc/my_info.html')
--- app/board/routes.py
+++ app/board/routes.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template
 
 board_bp = Blueprint('board', __name__, url_prefix='/board')
 
+
 @board_bp.route('/index')
 def index():
     return render_template('board/index.html')
+
+
+@board_bp.route('/create_post')
+def create_post():
+    return render_template('board/create_post.html')
