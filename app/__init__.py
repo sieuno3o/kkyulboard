5c1dac4f9bd6a5ee044a5d874b37bd8f06a09189
sang-il <lsi9393@naver.com>
2024-04-01 19:36:51 +0900

    feat board * User, Post 사이 관계 설정 * 게시판 화면 UI/UX 1차 적용

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,32 @@ from .acc.routes import acc_bp
 from .board.routes import board_bp
 import os
 
+
 def create_app():
-        app = Flask(__name__, template_folder='../templates')
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        app.config['SQLALCHEMY_DATABASE_URI'] =\
-                'sqlite:///' + os.path.join(basedir, 'database.db')
-        db.init_app(app)
+    app = Flask(__name__, template_folder='../templates')
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+    db.init_app(app)
 
-        app.register_blueprint(acc_bp)
-        app.register_blueprint(board_bp)
+    app.register_blueprint(acc_bp)
+    app.register_blueprint(board_bp)
 
-        return app
+    return app
+
+def create_app_with_dbname(dbname: str):
+    app = Flask(__name__, template_folder='../templates')
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'sqlite:///' + os.path.join(basedir, dbname)
+    db.init_app(app)
+
+    app.register_blueprint(acc_bp)
+    app.register_blueprint(board_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
--- app/board/routes.py
+++ app/board/routes.py
@@ -1,7 +1,50 @@
+import flask
+import kkyulboard.app.database
+from datetime import datetime
 from flask import Blueprint, render_template
+from ..database import *
 
 board_bp = Blueprint('board', __name__, url_prefix='/board')
 
+
 @board_bp.route('/index')
 def index():
+    posts_results = Post.query.all()
+    for post in posts_results:
+        post.updated_at = datetime.date(post.updated_at)
+    return render_template('board/index.html', posts=posts_results)
+
+
+@board_bp.route('/recent', methods=['GET'])
+def recent():
+    posts_results = Post.query.all()
+    return render_template('board/index.html', posts=posts_results)
+
+
+@board_bp.route('/oldest', methods=['GET'])
+def oldest():
+    posts_results = Post.query.all()
+    return render_template('board/index.html', posts=posts_results)
+
+@board_bp.route('/write', methods=['POST'])
+def write():
     return render_template('board/index.html')
+
+@board_bp.route('/test_data', methods=['POST'])
+def test_data():
+    print('test')
+    user = User.query.filter_by(username='user1').first()
+    if not user:
+        user = User(username='user1', password='1111', email='email1', grade='', userpic='')
+        db.session.add(user)
+        db.session.commit()
+
+    post = Post(title='post1', body='body1', user_id=user.id, created_at=datetime.now(),
+                updated_at=datetime.now(), click_count=1)
+
+    db.session.add(post)
+    db.session.commit()
+
+    posts_results = Post.query.all()
+
+    return render_template('board/index.html', posts=posts_results)
